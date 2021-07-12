1eb04c7f3b22f40351f6c26c1a6dee1ff2310b02
sb7297 <sbaireddy7297@gmail.com>
2021-07-12 13:58:25 -0400

    Todos: render static data

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,33 @@
 import React, { useState } from 'react';
 
 export default function Todos() {
+  const [todos, setTodos] = useState([
+    {id: 1, text: 'Create Todos component file', complete: true},
+    {id: 2, text: 'Finish Todos stretch goal', complete: false}
+  ]);
+
+  const incompleteStyle = {
+    margin: '0.5em',
+    color: 'red',
+  };
+
+  const completeStyle = {
+    margin: '0.5em',
+    textDecoration: 'line-through'
+  };
+
 	return (
 		<div className='widget-todos container'>
       <h2>Todos</h2> 
+      <div className='todos'>
+        {
+          todos.map(item => 
+            <div className='todo' key={item.id} style={item.complete ? completeStyle : incompleteStyle}>
+              {item.text}
+            </div>
+          )
+        }
+      </div>
 		</div>
 	);
 }
